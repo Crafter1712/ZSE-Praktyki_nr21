065d7bbf4ad62cf290619004691762896a896ac1
Crafter1712 <79221895+Crafter1712@users.noreply.github.com>
2021-03-02 11:52:20 +0100

    Add files via upload

---
+++ 2.03.2021/1/Main.java
@@ -0,0 +1,9 @@
+package pl.Skalinski;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        new pojazd("V8","red",2);
+    }
+}
---
+++ 2.03.2021/1/pojazd.java
@@ -0,0 +1,24 @@
+package pl.Skalinski;
+
+public class pojazd {
+    private String silnik;
+    private String kolor;
+    private double rozmiar;
+    public String model;
+
+    public String getModel(){
+        return model;
+    }
+
+    public void setModel(String model){
+        this.model = model;
+    }
+    public pojazd(String silnik, String kolor, double rozmiar){
+       this.silnik = silnik;
+       this.kolor = kolor;
+       this.rozmiar = rozmiar;
+       String model = "Stan pojazdu : " + silnik + "  silnik " + kolor +  "  kolor " + rozmiar + "  roziar ";
+        System.out.println(model);
+    }
+
+}
